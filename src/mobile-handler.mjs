bd4cb925d5d605d14881a47645327bb76f827148
Active <jaredbates123@yahoo.com>
2024-01-24 09:42:40 -0600

    added new API to detect information about the device

--- src/mobile-handler.mjs
+++ src/mobile-handler.mjs
@@ -82,12 +82,6 @@ class MobileHandlerSingleton {
 	 * @type {number}
 	 */
 	xCenterScreenPosition = this.windowSize.width / 2;
-	/**
-	 * Whether a mobile device was detected
-	 * @private
-	 * @type {boolean}
-	 */
-	isMobile = false;
 	constructor() {
         /** The logger module this module uses to log errors / logs
          * @private
@@ -95,14 +89,11 @@ class MobileHandlerSingleton {
          */
         this.logger = new Logger();
         this.logger.registerType('MobileHandler-Module', '#ff6600');
-		// Regex to check if it matches a mobile device
-		if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) {
-			this.isMobile = true;
-		}
 		// Create and show the mobile handler interface that our controllers will exist in
 		VYLO.Client.createInterface('mobile-handler-interface');
 		VYLO.Client.showInterface('mobile-handler-interface');
 
+		this.queryUserAgent();
 		this.registerEventHandlers();
 		this.querySafeAreaValues();
 	}
@@ -152,19 +143,13 @@ class MobileHandlerSingleton {
 		return this.safeAreaValues.right;
 	}
 	/**
-	 * Return the type of device.
-	 * @returns The type of device. Android / Ipad / Iphone
+	 * Detects the type of device based on user agent.
+	 *
+	 * @returns {string} The detected device type ('desktop' or 'mobile').
 	 */
-	getDevice() {
-		if ((navigator.userAgent.indexOf('iPhone') > 0 && navigator.userAgent.indexOf('iPad') == -1) || navigator.userAgent.indexOf('iPod') > 0 ) {
-			return 'iPhone'; 
-		} else if(navigator.userAgent.indexOf('iPad') > 0) {
-			return 'iPad';
-		} else if(navigator.userAgent.indexOf('Android') > 0) {
-			return 'Android';
-		} else {
-			return null;
-		}
+	detectDeviceType() {
+		const isMobile = /iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase());
+		return isMobile ? 'mobile' : 'desktop';
 	}
 	/**
 	 * Vibrates the mobile device.
@@ -178,6 +163,34 @@ class MobileHandlerSingleton {
 				navigator.vibrate(pDuration);
 			}
 		}
+	}
+	/**
+	 * If this device can register touch events.
+	 * @returns {boolean} - Whether this device is a touch enabled device.
+	 */
+	isTouchDevice() {
+		return this.isTouchEnabled;
+	}
+	/**
+	 * If this device is a mobile device.
+	 * @returns {boolean} - Whether this device is a mobile device.
+	 */
+	isMobileDevice() {
+		return this.isMobile;
+	}
+	/**
+	 * If this device is an ios device.
+	 * @returns {boolean} - Whether this device is an ios device.
+	 */
+	isIOSDevice() {
+		return this.isIOS;
+	}
+	/**
+	 * If this device is an android device.
+	 * @returns {boolean} - Whether this device is an android device.
+	 */
+	isAndroidDevice() {
+		return this.isAndroid;
 	}
 	/**
 	 * Gets the device's current RAM
@@ -201,7 +214,6 @@ class MobileHandlerSingleton {
 		}
 		return new Controller(pOptions);
 	}
-
 	/**
 	 * Handler event for when the window resizes.
 	 * @private
@@ -352,6 +364,32 @@ class MobileHandlerSingleton {
 			bottom: safeAreaInsetBottom,
 			left: safeAreaInsetLeft
 		}
+	}
+	queryUserAgent() {
+		/**
+		 * Check if the user agent string contains 'iPhone' or 'iPad'
+		 * @private
+		 * @type {boolean}
+		 */
+		this.isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
+		/**
+		 * Check if the user agent string contains 'Android'
+		 * @private
+		 * @type {boolean}
+		 */
+		this.isAndroid = /Android/.test(navigator.userAgent);
+		/**
+		 * Check if the 'ontouchstart' event is supported
+		 * @private
+		 * @type {boolean}
+		 */
+		this.isTouchEnabled = 'ontouchstart' in document.documentElement;
+		/**
+		 * Whether this device is mobile or not. Can be simulated.
+		 * @private
+		 * @type {boolean}
+		 */
+		this.isMobile = this.detectDeviceType() === 'mobile' ? true : false;
 	}
 	/**
 	 * Handles when a finger is placed onto the screen in a zone
